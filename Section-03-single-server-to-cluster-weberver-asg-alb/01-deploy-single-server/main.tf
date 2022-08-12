f7cf7cd11686665eaa1c669568356bd7ace254b0
DG0026 <93153436+DG0026@users.noreply.github.com>
2022-08-12 10:13:13 -0400

    Update main.tf

--- Section-03-single-server-to-cluster-weberver-asg-alb/01-deploy-single-server/main.tf
+++ Section-03-single-server-to-cluster-weberver-asg-alb/01-deploy-single-server/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "hello-world" {
 
  ami = "ami-0a313d6098716f372" 
  instance_type = "t2.micro"
- vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
+ vpc_security_group_ids = ["${aws_security_group.dg_webserver_sg.id}"]
  key_name = "terraform"
  tags = {
 	 Name = "Hello world"
